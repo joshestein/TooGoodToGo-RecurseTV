2b56b8905fd9fbe28e8284afeaf30acf7d47eaf7
Josh <josh.e.stein@gmail.com>
2024-05-23 10:59:44 -0400

    Read config vars from environment

--- config.py
+++ config.py
@@ -1,9 +1,15 @@
+import os
 from functools import lru_cache
 
 from dotenv import dotenv_values
 
 
 @lru_cache
-def get_config():
-    config = dotenv_values(".env")
+def get_config(keys=None):
+    if keys is None:
+        keys = ["ACCESS_TOKEN", "REFRESH_TOKEN", "USER_ID", "COOKIE", "OSM_API_KEY"]
+
+    fallback_values = dotenv_values(".env")
+    config = {key: os.environ.get(key, fallback_values.get(key)) for key in keys}
+
     return config
